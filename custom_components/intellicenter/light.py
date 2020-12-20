1bfcbe8e2171ad6c15e23b81fe97702dfdf3c0a5
Jean-Luc Vaillant <jvaillant@mac.com>
2020-12-19 17:38:00 -0800

    Update light.py
    Added light effects for IntelliBrite lights

--- custom_components/intellicenter/light.py
+++ custom_components/intellicenter/light.py
@@ -1,7 +1,7 @@
 import logging
 from typing import Any, Callable, Dict, List, Optional
 
-from homeassistant.components.light import LightEntity
+from homeassistant.components.light import LightEntity, SUPPORT_EFFECT, ATTR_EFFECT
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.typing import HomeAssistantType
 
@@ -23,6 +23,12 @@ async def async_setup_entry(hass, entry: ConfigEntry, async_add_entities):
             lights.append(PoolLight(entry, controller, object))
     async_add_entities(lights)
 
+LIGHTS_EFFECTS_BY_TYPE = {
+    'INTELLI': {
+        'PARTY': 'Party Mode',  'CARIB': 'Caribbean', 'SSET': 'Sunset', 'ROMAN': 'Romance', 'AMERCA': 'American',
+        'ROYAL': 'Royal', 'WHITER': 'White', 'REDR': 'Red', 'BLUER': 'Blue', 'GREENR': 'Green', 'MAGNTAR': 'Magenta'
+    }}
+
 class PoolLight(PoolEntity, LightEntity):
     """Representation of an Pentair light."""
 
@@ -30,6 +36,29 @@ class PoolLight(PoolEntity, LightEntity):
         super().__init__(entry, controller, poolObject)
         # USE appears to contain extra info like color...
         self._extraStateAttributes = ['USE']
+
+        self._features = 0
+
+        self._lightEffects = LIGHTS_EFFECTS_BY_TYPE.get(poolObject.subtype,{})
+        self._reversedLightEffects = dict(map(reversed,self._lightEffects.items()))
+
+        if self._lightEffects:
+            self._features |= SUPPORT_EFFECT
+
+    @property
+    def supported_features(self) -> int:
+        """Flag supported features."""
+        return self._features
+
+    @property
+    def effect_list(self) -> list:
+        """Return the list of supported effects."""
+        return list(self._reversedLightEffects.keys())
+
+    @property
+    def effect(self) -> str:
+        """Return the current effect."""
+        return self._lightEffects.get(self._poolObject['USE'])
 
     @property
     def is_on(self) -> bool:
@@ -41,5 +70,14 @@ class PoolLight(PoolEntity, LightEntity):
         self.requestChanges({'STATUS': 'OFF'})
 
     def turn_on(self, **kwargs: Any) -> None:
-        """Turn off the light."""
-        self.requestChanges({'STATUS': self._poolObject.onStatus})
+        """Turn on the light."""
+ 
+        changes = {'STATUS': self._poolObject.onStatus}
+
+        if ATTR_EFFECT in kwargs:
+            effect = kwargs[ATTR_EFFECT]
+            new_use = self._reversedLightEffects.get(effect)
+            if new_use:
+                changes['ACT'] = new_use
+        
+        self.requestChanges(changes)
