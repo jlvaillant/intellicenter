dc1e827f1f8a12b8c4cc6b9b0e577d2da5d6414c
Ryan Snodgrass <rsnodgrass@gmail.com>
2021-06-03 00:23:02 -0700

    update

--- custom_components/intellicenter/const.py
+++ custom_components/intellicenter/const.py
@@ -4,5 +4,3 @@ DOMAIN = "intellicenter"
 DEVICE_CLASS_ROTATION_SPEED = "rotational_speed"
 CONST_RPM = "rpm"  # rotation per minute
 CONST_GPM = "gpm"  # gallon per minute
-
-ATTR_ATTRIBUTION = 'attribution'
